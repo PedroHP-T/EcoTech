3aba0fc13f625bc9dae089b1e5dd5144f397b95e
Pedro <henriquepereirapedro13@gmail.com>
2025-11-18 13:34:46 -0300

    Projeto Finalizado - By 3ºA

--- Versão_06/projeto.py
+++ Versão_06/projeto.py
@@ -494,8 +494,8 @@ elif selected == "Opiniões":
             grafico_img = Image.open(BytesIO(response.content))
             if grafico_img.mode != "RGB":
                 grafico_img = grafico_img.convert("RGB")
-            # Aumenta o tamanho da imagem (ex: largura 700px)
-            st.image(grafico_img, width=700)
+            # Aumenta levemente a altura para deixar mais esticado verticalmente
+            st.image(grafico_img, use_column_width=True, height=500)  # ajuste a altura conforme quiser
         except Exception as e:
             st.write(f"Não foi possível carregar a imagem: {e}")
         st.markdown("</div>", unsafe_allow_html=True)
