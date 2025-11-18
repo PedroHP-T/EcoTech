179ee206cd6da521e95a4178241df079d351333b
Pedro <henriquepereirapedro13@gmail.com>
2025-11-18 16:27:27 -0300

    Projeto Finalizado - By 3ºA

--- Versão_06/projeto.py
+++ Versão_06/projeto.py
@@ -783,6 +783,7 @@ elif selected == "ChatBot":
             </style>
             """, unsafe_allow_html=True)
     st.markdown(f"<h2>EcoBot</h2>", unsafe_allow_html=True)
+    st.header("", divider="green")
     
     st.markdown("Fale com nosso assistente virtual especializado **apenas sobre descarte eletrônico e reciclagem tecnológica.**")
     
